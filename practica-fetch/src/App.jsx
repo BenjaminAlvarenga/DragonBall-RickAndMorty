fbd942b9ccf81e7c99cd47fa9366e7ef677a592f
BenjaminAlvarenga <20240080@ricaldone.edu.sv>
2026-04-16 11:23:48 -0600

    commit

---
+++ practica-fetch/src/App.jsx
@@ -0,0 +1,22 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router"
+import Home from "./pages/Home.jsx"
+import Nav from "./components/Nav.jsx"
+import DragonBall from "./pages/DragonBall.jsx"
+
+function App() {
+
+  return (
+    <>
+    <Router>
+        <Nav />
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/dragon' element={<DragonBall/>}></Route>
+          </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App
