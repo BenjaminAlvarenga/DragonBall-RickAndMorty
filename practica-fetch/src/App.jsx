4aed6a48fa7ded51be59448b6207438cd8f4e3e6
BenjaminAlvarenga <20240080@ricaldone.edu.sv>
2026-04-17 15:15:01 -0600

    terminao

--- practica-fetch/src/App.jsx
+++ practica-fetch/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router"
 import Home from "./pages/Home.jsx"
 import Nav from "./components/Nav.jsx"
 import DragonBall from "./pages/DragonBall.jsx"
+import RickAndMorty from "./pages/RickAndMorty.jsx"
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home/>}></Route>
             <Route path='/dragon' element={<DragonBall/>}></Route>
+            <Route path='/rickandmorty' element={<RickAndMorty/>}></Route>
           </Routes>
     </Router>
     </>
--- practica-fetch/src/components/Nav.jsx
+++ practica-fetch/src/components/Nav.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 function Nav(){
     return(
-        <nav className="flex items-center justify-between border-b border-slate-200 px-4 py-3 bg-gradient-to-r from-blue-500 to blue-900 text-white">
+        <nav className="flex items-center justify-between border-b border-slate-200 px-4 py-3 bg-gradient-to-r from-blue-500 to-blue-900 text-white">
             <h2 className="m-0 text-lg font-semibold text-white">React Fetching</h2>
             <div className="flex items-center gap-4 text-sm">
                 <Link to="/" className="hover:underline">
@@ -10,6 +10,9 @@ function Nav(){
                 </Link>
                 <Link to="/dragon" className="hover:underline">
                 DragonBall
+                </Link>
+                <Link to="/rickandmorty" className="hover:underline">
+                Rick and Morty
                 </Link>
             </div>
         </nav>
